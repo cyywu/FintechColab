ecfd966bc7ee557ad737aae452f5e1838ab99d2d
calvin wu <yinyu@connect.hku.hk>
2020-07-17 16:23:16 +0800

    Q3 ready

--- MonetaryBaseVisualiser.py
+++ MonetaryBaseVisualiser.py
@@ -3,6 +3,8 @@
 # !{sys.executable} -m pip install matplotlib >/dev/null 2>&1
 
 import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
 import urllib.request
 import json
 from datetime import datetime
@@ -79,7 +81,7 @@ def MBC():
     plt.show()
 
 
-MBC()
+# MBC()
 
 
 # ---------------- 1. Money Multiplier = M3 / MB (HKD) ----------------
@@ -129,7 +131,7 @@ def MM():
         formatted_mb_total_Y += [mbc_records[i]['mb_bf_disc_win_total']]
 
     # Plot mm
-    plt.plot(formatted_X, formatted_mm_Y, color='orange', linewidth=4, alpha=0.7, label="money multiplier")
+    plt.plot(formatted_X, formatted_mm_Y, color='#990DAD', linewidth=4, alpha=0.7, label="money multiplier")
     plt.title("1. Money Multiplier = M3 / MB (HKD)")
     plt.xlabel("End of month")
     plt.ylabel("m3 to mb ratio")
@@ -139,7 +141,7 @@ def MM():
     plt.show()
 
     # Plot m3
-    plt.plot(formatted_X, formatted_m3_total_Y, color='red', linewidth=4, alpha=0.7, label="m3")
+    plt.plot(formatted_X, formatted_m3_total_Y, color='#E30D55', linewidth=4, alpha=0.7, label="m3")
     plt.title("M3 Total")
     plt.xlabel("End of month")
     plt.ylabel("HK$ million")
@@ -149,7 +151,7 @@ def MM():
     plt.show()
     
     # Plot mb
-    plt.plot(formatted_X, formatted_mb_total_Y, color='blue', linewidth=4, alpha=0.7, label="mb")
+    plt.plot(formatted_X, formatted_mb_total_Y, color='#6FE50E', linewidth=4, alpha=0.7, label="mb")
     plt.title("Total Monetary Base before Discount Window")
     plt.xlabel("End of month")
     plt.ylabel("HK$ million")
@@ -204,7 +206,7 @@ def TDHKD():
         formatted_tdhkd_Y += [td_records[i]['deposits_hkd']]
 
     # Plot tdhkd
-    plt.plot(formatted_X, formatted_tdhkd_Y, color='green',
+    plt.plot(formatted_X, formatted_tdhkd_Y, color='#F05900',
              linewidth=4, alpha=0.7, label="m3")
     plt.title("2. Total deposits (HKD)")
     plt.xlabel("End of month")
@@ -215,7 +217,7 @@ def TDHKD():
     plt.show()
 
     # Plot m3
-    plt.plot(formatted_X, formatted_m3_total_Y, color='red',
+    plt.plot(formatted_X, formatted_m3_total_Y, color='#00C4C4',
              linewidth=4, alpha=0.7, label="m3")
     plt.title("M3 Total")
     plt.xlabel("End of month")
@@ -226,32 +228,146 @@ def TDHKD():
     plt.show()
 
     # Plot tdhkd and m3
-    fig, ax1 = plt.subplots()
-
-    color = 'tab:green'
-    ax1.set_xlabel("End of month")
-    ax1.set_ylabel("HK$ million", color=color)
-    ax1.set_xlabel('time (s)')
-    ax1.plot(formatted_X, formatted_tdhkd_Y, color=color, label="Total deposits (HKD)")
-    ax1.tick_params(axis='y', labelcolor=color)
-    
-    ax1.set_xticks([formatted_X[-1], '2016-06', '2012-06',
+    plt.plot(formatted_X, formatted_tdhkd_Y, color='#F05900', label="Total deposits (HKD)")
+    plt.plot(formatted_X, formatted_m3_total_Y, color='#00C4C4', label="M3 Total")
+    plt.title("Total deposits (HKD) and M3 Total Correlation")
+    plt.xlabel("End of month")
+    plt.ylabel("HK$ million")
+    plt.xticks([formatted_X[-1], '2016-06', '2012-06',
                 '2008-06', '2004-06', '1998-09'])
-
-    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-
-    color = 'tab:red'
-    ax2.set_ylabel("HK$ million", color=color)
-    ax2.plot(formatted_X, formatted_m3_total_Y, color=color, label="M3 Total")
-    ax2.tick_params(axis='y', labelcolor=color)
-
-    fig.tight_layout()  # otherwise the right y-label is slightly clipped
-    ax2.set_xticks([formatted_X[-1], '2016-06', '2012-06',
-                '2008-06', '2004-06', '1998-09'])
-
-    ax1.legend(loc='upper left')
-    ax2.legend(loc='center left')
-    ax1.set_title("Total deposits (HKD) and M3 Total Correlation")
+    plt.legend(loc='upper left')
     plt.show()
 
 TDHKD()
+
+# ---------------- 3. Loan to Deposits ----------------
+def get_BS_from_HKMA():
+    # Balance sheet
+    url = 'https://api.hkma.gov.hk/public/market-data-and-statistics/monthly-statistical-bulletin/banking/balance-sheet-ais?choose=end_of_month&from=1998-09&to=no&sortby=end_of_month&sortorder=asc'
+
+    # retrieve data from hkma api, 100 entries per call, untill every data are retrieveed
+    records = []
+    offset = 0
+    while True:
+        with urllib.request.urlopen(url+"&offset="+str(offset), context=context) as req:
+            data = json.loads(req.read().decode())
+            records += data['result']['records']
+        if data['result']['datasize'] <= 0:
+            break
+        else:
+            offset += 100
+
+    # now data retrieved from hkma are stored in 'records' in a chronological order
+    return records
+
+
+def LtoD_hkd():
+    bs_records = get_BS_from_HKMA() 
+    
+    formatted_X = []
+    formatted_liab_depcust_hkd = []
+    formatted_asst_loanadvtocust_hkd = []
+    formatted_LtoD_hkd = []
+    
+    for record in bs_records:
+        formatted_X += [ record['end_of_month'] ]
+        
+        formatted_asst_loanadvtocust_hkd += [ record['asst_loanadvtocust_hkd'] ]
+        formatted_liab_depcust_hkd += [ record['liab_depcust_hkd'] ]
+        formatted_LtoD_hkd += [ record['asst_loanadvtocust_hkd'] / record['liab_depcust_hkd'] ]
+
+    # Plot LtoD hkd
+    plt.plot(formatted_X, formatted_LtoD_hkd, color='#E369AE', linewidth=4, alpha=1, label="LtoD (HKD)")
+    plt.title("3.1. Loan to Deposits (HKD)")
+    plt.xlabel("End of month")
+    plt.ylabel("L to D ratio")
+    plt.xticks([formatted_X[-1], '2016-06', '2012-06',
+                '2008-06', '2004-06', '1998-09'])
+    plt.legend(loc='upper left')
+    plt.show()
+
+    plt.plot(formatted_X, formatted_asst_loanadvtocust_hkd, color='#FFFB76', label="Loan (HKD)")
+    plt.plot(formatted_X, formatted_liab_depcust_hkd, color='#F05900', label="Deposits (HKD)")
+    plt.title("Loan and Deposits (HKD)")
+    plt.xlabel("End of month")
+    plt.ylabel("HK$ million")
+    plt.xticks([formatted_X[-1], '2016-06', '2012-06',
+                '2008-06', '2004-06', '1998-09'])
+    plt.legend(loc='upper left')
+    plt.show()
+
+def LtoD_fc():
+    bs_records = get_BS_from_HKMA() 
+    
+    formatted_X = []
+    formatted_liab_depcust_fc = []
+    formatted_asst_loanadvtocust_fc = []
+    formatted_LtoD_fc = []
+
+    for record in bs_records:
+        formatted_X += [ record['end_of_month'] ]
+        formatted_asst_loanadvtocust_fc += [ record['asst_loanadvtocust_fc'] ]
+        formatted_liab_depcust_fc += [ record['liab_depcust_fc'] ]
+        formatted_LtoD_fc += [ record['asst_loanadvtocust_fc'] / record['liab_depcust_fc'] ]
+
+    # Plot LtoD fc
+    plt.plot(formatted_X, formatted_LtoD_fc, color='#E369AE', linewidth=4, alpha=1, label="LtoD (FC)")
+    plt.title("3.2. Loan to Deposits (FC)")
+    plt.xlabel("End of month")
+    plt.ylabel("L to D ratio")
+    plt.xticks([formatted_X[-1], '2016-06', '2012-06',
+                '2008-06', '2004-06', '1998-09'])
+    plt.legend(loc='upper left')
+    plt.show()
+
+    plt.plot(formatted_X, formatted_asst_loanadvtocust_fc, color='#FFFB76', label="Loan (FC)")
+    plt.plot(formatted_X, formatted_liab_depcust_fc, color='#F05900', label="Deposits (FC)")
+    plt.title("Loan and Deposits (FC)")
+    plt.xlabel("End of month")
+    plt.ylabel("HK$ million")
+    plt.xticks([formatted_X[-1], '2016-06', '2012-06',
+                '2008-06', '2004-06', '1998-09'])
+    plt.legend(loc='upper left')
+    plt.show()
+
+
+def LtoD_total():
+    bs_records = get_BS_from_HKMA() 
+    
+    formatted_X = []
+    formatted_liab_depcust_total = []
+    formatted_asst_loanadvtocust_total = []
+    formatted_LtoD_total = []
+    
+    for record in bs_records:
+        formatted_X += [ record['end_of_month'] ]
+
+        formatted_asst_loanadvtocust_total += [ record['asst_loanadvtocust_total'] ]
+        formatted_liab_depcust_total += [ record['liab_depcust_total'] ]
+        formatted_LtoD_total += [ record['asst_loanadvtocust_total'] / record['liab_depcust_total'] ]
+
+    # Plot LtoD total
+    plt.plot(formatted_X, formatted_LtoD_total, color='#E369AE', linewidth=4, alpha=1, label="LtoD (Total)")
+    plt.title("3.2. Loan to Deposits (Total)")
+    plt.xlabel("End of month")
+    plt.ylabel("L to D ratio")
+    plt.xticks([formatted_X[-1], '2016-06', '2012-06',
+                '2008-06', '2004-06', '1998-09'])
+    plt.legend(loc='upper left')
+    plt.show()
+
+    plt.plot(formatted_X, formatted_asst_loanadvtocust_total, color='#FFFB76', label="Loan (Total)")
+    plt.plot(formatted_X, formatted_liab_depcust_total, color='#F05900', label="Deposits (Total)")
+    plt.title("Loan and Deposits (Total)")
+    plt.xlabel("End of month")
+    plt.ylabel("HK$ million")
+    plt.xticks([formatted_X[-1], '2016-06', '2012-06',
+                '2008-06', '2004-06', '1998-09'])
+    plt.legend(loc='upper left')
+    plt.show()
+
+LtoD_hkd()
+
+LtoD_fc()
+
+LtoD_total()
