2187384f1d63bdcb8a92487a45a519340d600bcb
calvin wu <yinyu@connect.hku.hk>
2020-07-25 00:33:51 +0800

    FHoTS done

--- FHoTS.py
+++ FHoTS.py
@@ -1,7 +1,5 @@
-# MAJOR FOREIGN HOLDERS OF TREASURY SECURITIES
-
 fixed_plot_width = True # set it to False and click Runtime > Run all if you prefer a responsive width
-plot_width = 600 # customise plot_width and click Runtime > Run all if you prefer a plot_width other than 600px
+plot_width = 1200 # customise plot_width and click Runtime > Run all if you prefer a plot_width other than 600px
 
 
 import matplotlib.pyplot as plt
@@ -21,6 +19,8 @@ import re
 context = ssl._create_unverified_context()
 
 # ---------------------------------------------------------------
+fhots_by_country = {}
+
 url = "https://ticdata.treasury.gov/Publish/mfhhis01.txt"
 csv = pd.read_csv("https://ticdata.treasury.gov/Publish/mfhhis01.txt", sep="\t", names=["data"])
 df = pd.DataFrame(csv)
@@ -50,10 +50,6 @@ for i in range(len(startPoints)):
   if(endPoints[i]):
     endBefores += [i]
 
-print('-------')
-
-
-# trying
 for i_table in range(len(startAts)):
   # get the table of the year
   table = df.iloc[startAts[i_table]:endBefores[i_table]]
@@ -63,6 +59,7 @@ for i_table in range(len(startAts)):
   
   countryNames = []
   datas = []
+  r = re.compile("(  )")
   # for each row of this table
   for i_row in range(endBefores[i_table] - startAts[i_table]):
     # split it from a string to a list of data, and remove n.a.
@@ -72,10 +69,9 @@ for i_table in range(len(startAts)):
     # to 
     #     ['Japan', ' ' , ' ' , ' ', '378.1', '374.9', ...]
     #     ['Luxembourg', ' ' , ' ' , ' ', ' 23.9', '23.7', ... , '0', '0', '0', '0', '0', '0']
-    r = re.compile("(n.a.)")
     dataRow = df_table['data'][i_row + startAts[i_table]]
-    removeNA = r.sub("  0", dataRow)
-    r = re.compile("(  )")
+    removeNA = re.sub('n\.a\.', '  0', dataRow)    
+    # subDataRow = r.sub("\t", dataRow)
     subDataRow = r.sub("\t", removeNA)
     splitDataRow = subDataRow.split("\t")
 
@@ -91,8 +87,6 @@ for i_table in range(len(startAts)):
     for key in toDelete:
         splitDataRow.pop(key)
 
-    print(splitDataRow)    
-    
     # deal with special case
     # from 
     #     ['Belgium', '5/', '13.0', '13.2', ... ]
@@ -127,12 +121,96 @@ for i_table in range(len(startAts)):
     for i_datasLen in range(len(datas)):
       datas[i_datasLen].pop(key)
 
-
+  # push data into fhots_by_country, ready for ploty
   for i in range(len(datas)):
     countryName = countryNames[i]
     dataListOfTheCountry = datas[i]
 
-    # for j in range(len(dataListOfTheCountry)):
-      # print([countryName, str(colsOfYears[i_table][j] + " " + years[i_table]), float(dataListOfTheCountry[j])])
-  
-  print('-------')
+    for j in range(len(dataListOfTheCountry)):
+      if not str(re.sub('[ ]*[1-9]*/', '', countryName)) in fhots_by_country:
+        fhots_by_country[str(re.sub('[ ]*[1-9]*/', '', countryName))] = []
+      
+      fhots_by_country[str(re.sub('[ ]*[1-9]*/', '', countryName))].append(
+          [str(colsOfYears[i_table][j] + " " + years[i_table]), float(dataListOfTheCountry[j])]
+      )
+
+fhots_dfs = {}
+for country in fhots_by_country:
+  fhots_dfs[country] = pd.DataFrame(fhots_by_country[country])
+  fhots_dfs[country].columns = ['date', str('fhots_'+ re.sub(' ','_', country))]
+
+fhots = fhots_dfs["Japan"]
+for country in fhots_by_country:
+    if not country == "Japan":
+      # print(fhots_dfs[country])
+      fhots = fhots.merge(fhots_dfs[country], how='outer').fillna(method='ffill')
+
+fhots.date = pd.to_datetime(fhots.date)
+dfColumns = list(fhots) 
+for col in dfColumns: 
+  if not col == 'date':
+    fhots[col] = pd.to_numeric(fhots[col]) * 1000000000
+
+
+# plot consolidated chart
+fig = go.Figure()
+fig.update_xaxes(
+  rangeselector=dict(
+      buttons=list([
+          dict(count=1, label="1y", step="year", stepmode="backward"),
+          dict(count=3, label="3y", step="year", stepmode="backward"),
+          dict(count=6, label="6y", step="year", stepmode="backward"),
+          dict(count=10, label="10y", step="year", stepmode="backward"),
+          dict(count=15, label="15y", step="year", stepmode="backward"),
+          dict(step="all")
+      ])
+    )
+)
+for col in dfColumns: 
+  if not col == 'date':
+    fig.add_trace(go.Scatter(x=fhots.date, y=fhots[col], mode='lines', name=(re.sub('_',' ',re.sub('fhots_','',col))), line=dict(width=1.5)))
+
+fig.update_layout(
+    title_text="<b>Foreign Holders of US Treasury Securities</b>",
+    xaxis_title='End of period <b>(Monthly)</b>',
+    yaxis_title='<b>USD</b> US Dollars',
+    height=700,
+)
+if fixed_plot_width:
+  fig.update_layout(width=plot_width)
+fig.show()
+
+
+# plot stacked 100% chart
+fig = go.Figure()
+fig.update_xaxes(
+  rangeselector=dict(
+      buttons=list([
+          dict(count=1, label="1y", step="year", stepmode="backward"),
+          dict(count=3, label="3y", step="year", stepmode="backward"),
+          dict(count=6, label="6y", step="year", stepmode="backward"),
+          dict(count=10, label="10y", step="year", stepmode="backward"),
+          dict(count=15, label="15y", step="year", stepmode="backward"),
+          dict(step="all")
+      ])
+    )
+)
+for col in dfColumns: 
+  if not col == 'date':
+    fig.add_trace(go.Scatter(x=fhots.date, y=fhots[col], mode='lines', line=dict(width=0.5), stackgroup='fhots', name=(re.sub('_',' ',re.sub('fhots_','',col))), groupnorm='percent'))
+
+fig.update_layout(
+    title_text="<b>Composition of FHoTS (100% stacked)</b>",
+    xaxis_title='End of period <b>(Monthly)</b>',
+    yaxis_title='<b>%</b> Percentage',
+    yaxis=dict(
+      type='linear',
+      range=[1, 100],
+      ticksuffix='%'
+    ),
+    height=700,
+    showlegend=True,
+)
+if fixed_plot_width:
+  fig.update_layout(width=plot_width)
+fig.show()
